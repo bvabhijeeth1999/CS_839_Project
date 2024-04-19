7b8dfed497d3d3cde5ff807e864b121f6913147e
bvabhijeeth1999 <bvabhi24@gmail.com>
2024-04-18 20:36:50 -0500

    tiktoken and main file common implementation

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import sys
+from tik_token import get_tokens_tik_token
+import pandas as pd
+
+def read_headlines_from_csv(file_path):
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv(file_path, header=None)
+    
+    # Extract headlines from the first column
+    headlines = df.iloc[:, 0].tolist()
+    
+    return headlines
+
 
 def main():
     # Check if the correct number of arguments is provided
@@ -8,8 +20,45 @@ def main():
     
     # Extract command-line arguments
     csv_file = sys.argv[1]
-    target_headline = sys.argv[2]
+    target_file = sys.argv[2]
     slo = sys.argv[3]
+
+    list_sentences = read_headlines_from_csv(csv_file)
+    target_headlines = read_headlines_from_csv(target_file)
+    target_headline = target_headlines[0]
+
+    for sentence in list_sentences:
+        print(sentence)
+
+    print("Printing target headline")
+    print(target_headline)
+    
+    # Sample run command : 
+    # python3 main.py input_list_sentences.csv target_sentence.csv 3
+
+    # sample list_sentences = [
+    #     "Fed official says weak data caused by weather, should not slow taper",
+    #     "Fed's Charles Plosser sees high bar for change in pace of tapering",
+    #     "US open: Stocks fall after Fed official hints at accelerated tapering",
+    #     "Fed risks falling 'behind the curve', Charles Plosser says",
+    #     "Fed's Plosser: Nasty Weather Has Curbed Job Growth",
+    #     "Plosser: Fed May Have to Accelerate Tapering Pace",
+    #     "Fed's Plosser: Taper pace may be too slow",
+    #     "Fed's Plosser expects US unemployment to fall to 6.2% by the end of 2014",
+    #     "US jobs growth last month hit by weather:Fed President Charles Plosser",
+    #     "ECB unlikely to end sterilisation of SMP purchases - traders" ,
+    #     "ECB unlikely to end sterilization of SMP purchases: traders",
+    #     "EU's half-baked bank union could work",
+    #     "Europe reaches crunch point on banking union",
+    #     "ECB FOCUS-Stronger euro drowns out ECB's message to keep rates low for  ...",
+    #     "EU aims for deal on tackling failing banks"
+    # ]
+
+
+    result_tik_token = []
+    # calling tik token tokenizer.
+    print("Calling tik token tokenizer")
+    result_tik_token = get_tokens_tik_token(list_sentences, target_headline)
     
     # now once we have the models, we need to iterate through those and match with slo requirements and trigger accordingly.
 
@@ -17,7 +66,7 @@ def main():
     # Then we will do the same for the target headline.
     # Then we will compare these arrays using some distance function and return top 5/10 or something.
 
-    # Tokenizers : N-Gram, TikToken, No Tokenizer
+    # Tokenizers (function name : get_tokens) : N-Gram, TikToken, No Tokenizer
     # String Matching Algorithms : TF-IDF, Jaccard, Edit-Distance (optional), ML (maybe after checkin)
 
     # Tokenizers Input : list of sentences + target sentence.
--- tik_token.py
+++ tik_token.py
@@ -1,20 +1,30 @@
 import tiktoken
-import sys
 
-# load an encoding by name
-encoding = tiktoken.get_encoding("cl100k_base")
+def get_tokens_tik_token(list_sentences, target_sentence):
 
-# to convert string into a list of token integers.
-token_list = encoding.encode(sys.argv[1])
+    # load an encoding by name
+    encoding = tiktoken.get_encoding("cl100k_base")
 
-# print(token_list)
-# print(len(token_list))
+    output_list_sentences = []
+    output_target_sentence = []
+    # to convert string into a list of token integers.
+    # Now, here need to iterate through the list of sentences and generate tokens for each sentence.
+    for sentence in list_sentences:
+        token_list = encoding.encode(sentence)
+        tokens = [encoding.decode_single_token_bytes(token).decode('utf-8') for token in token_list]
+        output_list_sentences.append((sentence,tokens))
 
-tokens = [encoding.decode_single_token_bytes(token) for token in token_list]
+    
+    token_list = encoding.encode(target_sentence)
+    tokens = [encoding.decode_single_token_bytes(token).decode('utf-8') for token in token_list]
+    output_target_sentence.append((sentence,tokens))
+    
+    print("\nprinting list of sentences with tokens from tiktoken\n")
+    for tup in output_list_sentences:
+        print(tup)
 
-# file_path = "tt_tokens.txt"
-# with open(file_path, 'w') as file:
-#     for item in tokens:
-#         file.write(str(item) + '\n')
-
-# print(tokens)
+    print("\nPrinting target sentence tokens\n")
+    for tup in output_target_sentence:
+        print(tup)
+    
+    return (output_list_sentences, output_target_sentence)
